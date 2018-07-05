014ad8cf427c06f2b7bad10b0d16c8cb5e3048d0
Robert Lathrop <rl@polyswarm.io>
2018-07-05 10:00:54 -0400

    Handle pylint warnings.

--- mainpython.py
+++ mainpython.py
@@ -1,14 +1,14 @@
-import requests
 import os
 import csv
 import glob
-
 from heapq import heappush, heappop
+
+import requests
 from web3 import Web3, HTTPProvider
 from web3.middleware import geth_poa_middleware
 
-keydir = "./keystore"
-truthcsv = "./artifacts/truth.csv"
+KEYDIR = "./keystore"
+TRUTHCSV = "./artifacts/truth.csv"
 
 polyswarmd = os.environ.get("POLYSWARMD")
 geth = os.environ.get("GETH")
@@ -18,30 +18,29 @@ password = os.environ.get("PASSWORD")
 w3 = Web3(HTTPProvider(geth))
 w3.middleware_stack.inject(geth_poa_middleware, layer=0)
 
-def decrypt_key(address, password):
-    os.chdir(keydir)
-    if address.starts_with('0x'):
-        temp = address[2:]
+def decrypt_key(addr, secret):
+    os.chdir(KEYDIR)
+    if addr.starts_with('0x'):
+        temp = addr[2:]
     possible_matches = glob.glob("*" + temp)
     os.chdir('..')
     if len(possible_matches) == 1:
         with open(possible_matches[0]) as keyfile:
             encrypted = keyfile.read()
-            return w3.decrypt_key(encrypted, password)
+            return w3.eth.account.decrypt(encrypted, secret)
     return None
 
 # Get the file hash from ipfs
 def get_artifacts(uri):
-    response = requests.get( polyswarmd + "/artifacts/" + uri )
+    response = requests.get(polyswarmd + "/artifacts/" + uri)
     decoded = response.json()
     if decoded["status"] == "OK":
         return list(map(lambda f: f["hash"], decoded["result"]))
-    else:
-        return list()
+    return list()
 
 # Finds the file in the truth table. Returns a boolean or None.
 def find_value_in_truth_table(filehash):
-    with open(truthcsv, newline='') as ground_truth:
+    with open(TRUTHCSV, newline='') as ground_truth:
         reader = csv.reader(ground_truth, delimeter=",")
         for row in reader:
             if row[0] == filehash:
@@ -50,13 +49,13 @@ def find_value_in_truth_table(filehash):
 
 # This will settle any bounties
 def settle_bounties(heap, blocknumber):
-    while len(heap) > 0:
+    while heap:
         head = heap[0]
 
         #  For any bounties blocknumber exceeds the reveal & vote windows... settle
         if int(head[0]) < blocknumber:
             guid = head[1]
-            response = requests.post( polyswarmd + "/bounties/" + guid + "/settle" )
+            response = requests.post(polyswarmd + "/bounties/" + guid + "/settle")
             transactions = response.json()["result"]["transactions"]
             response = sign_transactions(transactions)
 
@@ -69,7 +68,7 @@ def settle_bounties(heap, blocknumber):
             break
 
 def vote(guid, verdicts):
-    response = requests.post( polyswarmd + "/bounties/" + guid + "/vote", json={"verdicts": verdicts, "valid_bloom": True} )
+    response = requests.post(polyswarmd + "/bounties/" + guid + "/vote", json={"verdicts": verdicts, "valid_bloom": True})
     transactions = response.json()["result"]["transactions"]
     response = sign_transactions(transactions)
     return response.json()["status"] == "OK"
@@ -81,17 +80,17 @@ def sign_transactions(transactions):
         signed = w3.eth.account.signTransaction(transaction, key)
         raw = bytes(signed["rawTransaction"]).hex()
         signed_transactions.append(raw)
-    return requests.post( polyswarmd + "/transactions", json={"transactions": signed_transactions} )
+    return requests.post(polyswarmd + "/transactions", json={"transactions": signed_transactions})
 
 # Listen to polyswarmd /bounties/pending route to find expired bounties
 def listen_and_arbitrate():
     # to_settle is a head of bounty objects ordered by block number when then assertion reveal phase ends
     to_settle = []
     voted_bounties = set()
-    while (True):
+    while True:
         # Check bounties route
         # TODO chain in env
-        response = requests.get( polyswarmd + "/bounties/pending" )
+        response = requests.get(polyswarmd + "/bounties/pending")
         decoded = response.json()
         if decoded["status"] == "OK":
             bounties = response.json()["result"]
@@ -99,7 +98,7 @@ def listen_and_arbitrate():
                 if bounty["guid"] not in voted_bounties:
                     # Vote
                     hashes = get_artifacts(bounty["artifactUri"])
-                    verdicts = list(map(lambda hash: find_value_in_truth_table(hash), hashes))
+                    verdicts = list(map(find_value_in_truth_table, hashes))
                     # If successfully volted, add to heap to be settled
                     if vote(bounty["guid"], verdicts):
                         # Mark voted
