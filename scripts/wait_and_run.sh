861024bf3bff9639386fbd8a2b22c6869e481ba8
Rob Lathrop <rl@polyswarm.io>
2018-08-28 15:54:52 -0400

    Use updated env vars.

--- scripts/wait_and_run.sh
+++ scripts/wait_and_run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 echo "starting..."
-./scripts/wait_for_it.sh $POLYSWARMD_HOST -t 0
+./scripts/wait_for_it.sh $POLYSWARMD_HOST:$POLYSWARMD_PORT -t 0
 ./scripts/wait_for_it.sh $API_KEY_HOST -t 0
 
 export API_KEY=$(./scripts/get_api_key.sh)
-export POLYSWARMD_HOST="http://${POLYSWARMD_HOST}"
 
 echo "arbiter API key: ${API_KEY}"
 
