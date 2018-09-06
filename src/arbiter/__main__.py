1a23f717ccd9767e0b2d55e151f39852545e639c
Maxwell Koo <mjkoo90@gmail.com>
2018-09-06 15:51:40 -0700

    Implement verbatim arbiter in terms of polyswarm-client

--- src/arbiter/__main__.py
+++ src/arbiter/__main__.py
@@ -1,19 +1,77 @@
-import sys
-import argparse
+import click
 import importlib
-import asyncio
+import logging
+import sys
 
-from arbiter import listen_and_arbitrate
+from arbiter.verbatim import VerbatimArbiter
 
-def main():
-    parser = argparse.ArgumentParser(description="Run an arbiter backend.")
+def choose_backend(backend):
+    """Resolves arbiter name string to implementation
 
-    parser.add_argument("--backend", help="Select the backend", default="verbatim")
-    parser.add_argument("--test", help="Exits on successful settle", action="store_true")
-    args = parser.parse_args()
+    Args:
+        backend (str): Name of the backend to load, either one of the predefined
+            implementations or the name of a module to load
+            (module:ClassName syntax or default of module:Arbiter)
+    Returns:
+        (Class): Arbiter class of the selected implementation
+    Raises:
+        (Exception): If backend is not found
+    """
+    arbiter_class = None
+    if backend == 'verbatim':
+        arbiter_class = VerbatimArbiter
+    else:
+        import_l = backend.split(":")
+        arbiter_module_s = import_l[0]
 
-    backend = importlib.import_module("backends.{0}".format(args.backend))
-    asyncio.get_event_loop().run_until_complete(listen_and_arbitrate(args.test, backend))
+        arbiter_module = importlib.import_module(arbiter_module_s)
+        arbiter_class = arbiter_module.Arbiter if ":" not in backend else getattr(arbiter_module, import_l[1])
+
+    if arbiter_class is None:
+        raise Exception("No arbiter backend found {0}".format(backend))
+
+    return arbiter_class
+
+
+@click.command()
+@click.option('--log', default='INFO',
+        help='Logging level')
+@click.option('--polyswarmd-addr', envvar='POLYSWARMD_ADDR', default='localhost:31337',
+        help='Address (host:port) of polyswarmd instance')
+@click.option('--keyfile', envvar='MICROENGINE_KEYFILE', type=click.Path(exists=True), default='keyfile',
+        help='Keystore file containing the private key to use with this microengine')
+@click.option('--password', envvar='MICROENGINE_PASSWORD', prompt=True, hide_input=True,
+        help='Password to decrypt the keyfile with')
+@click.option('--api-key', envvar='API_KEY', default='',
+        help='API key to use with polyswarmd')
+@click.option('--backend', envvar='MICROENGINE_BACKEND', default='scratch',
+        help='Backend to use')
+@click.option('--testing', default=-1,
+        help='Activate testing mode for integration testing, respond to N bounties then exit')
+@click.option('--insecure-transport', is_flag=True,
+        help='Connect to polyswarmd via http:// and ws://, mutially exclusive with --api-key')
+def main(log, polyswarmd_addr, keyfile, password, api_key, backend, testing, insecure_transport):
+    """Entrypoint for the arbiter driver
+
+    Args:
+        polyswarmd_addr(str): Address of polyswarmd
+        keyfile (str): Path to private key file to use to sign transactions
+        password (str): Password to decrypt the encrypted private key
+        backend (str): Backend implementation to use
+        api_key(str): API key to use with polyswarmd
+        testing (int): Mode to process N bounties then exit (optional)
+        insecure_transport (bool): Connect to polyswarmd without TLS
+    """
+
+    loglevel = getattr(logging, log.upper(), None)
+    if not isinstance(loglevel, int):
+        logging.error('invalid log level')
+        sys.exit(-1)
+    logging.basicConfig(level=loglevel)
+
+    arbiter_class = choose_backend(backend)
+    arbiter_class(polyswarmd_addr, keyfile, password, api_key, testing, insecure_transport).run()
+
 
 if __name__ == "__main__":
     main()
---
+++ src/arbiter/verbatim.py
@@ -0,0 +1,21 @@
+import sqlite3
+import hashlib
+import os
+
+from polyswarmclient.arbiter import Arbiter
+
+class VerbatimArbiter(Arbiter):
+    def __init__(self, polyswarmd_addr, keyfile, password, api_key=None, testing=False, insecure_transport=False):
+        super().__init__(polyswarmd_addr, keyfile, password, api_key, testing, insecure_transport)
+        self.conn = sqlite3.connect(os.path.join(os.getcwd(), "artifacts", "truth.db"))
+
+    async def scan(self, guid, content):
+        h = hashlib.sha256(content).hexdigest()
+        return self.find_truth(h)
+
+    # Finds the file in the truth table. Returns a boolean or None.
+    def find_truth(self, filehash):
+        cursor = self.conn.cursor()
+        cursor.execute('SELECT * FROM files WHERE name=?', (filehash,))
+        row = cursor.fetchone()
+        return row is not None and row[1] == 1
