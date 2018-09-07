5dd91bf139f958fbb53a23ac982528335d140093
Rob Lathrop <rl@polyswarm.io>
2018-09-07 10:50:06 -0400

    Use click over argparse.

--- src/arbiter/__main__.py
+++ src/arbiter/__main__.py
@@ -1,19 +1,19 @@
 import sys
-import argparse
 import importlib
 import asyncio
+import click
 
 from arbiter import listen_and_arbitrate
 
-def main():
-    parser = argparse.ArgumentParser(description="Run an arbiter backend.")
+@click.command()
+@click.option('--backend', default='verbatim',
+        help='Which backend to use when scanning files.')
+@click.option('--test', is_flag=True, default=False,
+        help='Exit on errors or successful settle.')
+def main(backend, test):
 
-    parser.add_argument("--backend", help="Select the backend", default="verbatim")
-    parser.add_argument("--test", help="Exits on successful settle", action="store_true")
-    args = parser.parse_args()
-
-    backend = importlib.import_module("backends.{0}".format(args.backend))
-    asyncio.get_event_loop().run_until_complete(listen_and_arbitrate(args.test, backend))
+    loaded_backend = importlib.import_module("backends.{0}".format(backend))
+    asyncio.get_event_loop().run_until_complete(listen_and_arbitrate(test, loaded_backend))
 
 if __name__ == "__main__":
     main()
